f201c2db13de8fc9b4721443c0f29bd2ab4a5ed8
Shahoriar Nazim Rifat <snrifat@incieto.com>
2023-11-20 05:29:21 -0800

    change footer credit

--- theme.config.tsx
+++ theme.config.tsx
@@ -11,7 +11,7 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Copyright &#169 2023- Incieto. All right reserved.',
   },
 }
 
